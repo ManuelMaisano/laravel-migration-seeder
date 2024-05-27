f21052d6ca0404c5d817adc83b1927a8020b1dcc
Manuel Maisano <manumaisano@gmail.com>
2024-05-27 17:30:55 +0200

    add model and controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,11 @@
 @section('content')
 <main>
     <h1>Home page</h1>
+    <ul>
+        @foreach ($trains as $train)
+            <li>{{ $train->azienda }}</li>
+        @endforeach
+    </ul>
 </main>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TrainController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+
+
+Route::get('/', [TrainController::class, 'index'])->name('home');
