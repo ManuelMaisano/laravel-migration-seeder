2b4c45b5348640f44da31e322e900e467c1be239
Manuel Maisano <manumaisano@gmail.com>
2024-05-27 16:47:25 +0200

    create table info

--- database/migrations/2024_05_27_143148_create_trains_table.php
+++ database/migrations/2024_05_27_143148_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza',130);
+            $table->string('stazione_di_arrivo',130);
+            $table->time('ora_di_partenza');
+            $table->time('ora_di_arrivo');
+            $table->string('codice_treno', 10);
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
