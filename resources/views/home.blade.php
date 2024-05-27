dc9dafddd2b20b8bd16a7e4679affbc7d5b6436a
Manuel Maisano <manumaisano@gmail.com>
2024-05-27 18:55:16 +0200

    popolare db a mano

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,10 @@
     <ul>
         @foreach ($trains as $train)
             <li>{{ $train->azienda }}</li>
+            <li>{{ $train->stazione_di_partenza }}</li>
+            <li>{{ $train->stazione_di_arrivo }}</li>
+            <li>{{ $train->ora_di_partenza }}</li>
+            <li>{{ $train->ora_di_arrivo }}</li>
         @endforeach
     </ul>
 </main>
