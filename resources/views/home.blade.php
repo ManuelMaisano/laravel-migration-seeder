6b992e5064d555541f265a501cacc03adfb8a926
Manuel Maisano <manumaisano@gmail.com>
2024-05-27 18:56:51 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,10 @@
     <ul>
         @foreach ($trains as $train)
             <li>{{ $train->azienda }}</li>
-            <li>{{ $train->stazione_di_partenza }}</li>
-            <li>{{ $train->stazione_di_arrivo }}</li>
-            <li>{{ $train->ora_di_partenza }}</li>
-            <li>{{ $train->ora_di_arrivo }}</li>
+            <li>Stzione di partenza:{{ $train->stazione_di_partenza }}</li>
+            <li>Stazione di arrivo:{{ $train->stazione_di_arrivo }}</li>
+            <li>Partenza alle:{{ $train->ora_di_partenza }}</li>
+            <li>Ora di aarrivo:{{ $train->ora_di_arrivo }}</li>
         @endforeach
     </ul>
 </main>
